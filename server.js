7f00bf6a97d369ed0ce4d706632fd50d8fc5047c
nkennaA <nkenna@mit.edu>
2019-10-15 11:08:24 -0400

    about

---
+++ server.js
@@ -0,0 +1,14 @@
+
+var http = require('http');
+http.createServer(function (req, res) {
+res.writeHead(200, {'Content-Type': 'text/html'});
+var url = req.url;
+res.write('<h1>Hello World!<h1>'); 
+res.end(); 
+if(url==='/about'){
+    res.write(`<h1>about<h1>`);
+    res.end();
+}
+}).listen(8080, function(){
+ console.log("server start at port 8080");
+});
